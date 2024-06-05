05dfa4273c662e8a11863260a93ff420a893e18b
Gemenim <121296811+Gemenim@users.noreply.github.com>
2024-06-05 10:08:12 +0300

    WorkOnBugs

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,13 +1,62 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
+    [SerializeField] private float _delayTime = 0.5f;
+    [SerializeField] private float _delta = 1;
+
     private float _count = 0;
+    private bool _isOff = true;
+    private IEnumerator _timeCounter;
+
+    public Action Change;
 
     public float Count => _count;
 
-    public void TakeTeime(float time)
+    private void Start()
     {
-        _count += time;
+        _timeCounter = TimeCounter();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(_isOff)
+            {
+                _isOff = false;
+                StartCoroutine(_timeCounter);
+            }
+            else
+            {
+                _isOff = true;
+                StopCoroutine(_timeCounter);
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            _count = 0;
+            Change();
+        }
+    }
+
+    private void TakeTeime(float time)
+    {
+        _count += _delta;
+    }
+
+    private IEnumerator TimeCounter()
+    {
+        WaitForSeconds delayTime = new WaitForSeconds(_delayTime);
+
+        while (true)
+        {
+            TakeTeime(_delayTime);
+            Change();
+
+            yield return delayTime;
+        }
     }
 }
--- Assets/Scripts/CounterView.cs
+++ Assets/Scripts/CounterView.cs
@@ -1,54 +1,28 @@
-using System.Collections;
 using TMPro;
 using UnityEngine;
 
-[RequireComponent(typeof(Counter))]
 public class CounterView : MonoBehaviour
 {
+    [SerializeField] private Counter _counter;
     [SerializeField] private TextMeshProUGUI _text;
-    [SerializeField] private Switcher _switch;
-    [SerializeField] private float _delayTime;
-
-    private Counter _counter;
 
     private void Start()
     {
-        _counter = GetComponent<Counter>();
         _text.text = _counter.Count.ToString();
     }
 
     private void OnEnable()
     {
-        _switch.OnSwitch += ChangeTime;
+        _counter.Change += ChangeText;
     }
 
     private void OnDisable()
     {
-        _switch.OnSwitch -= ChangeTime;
+        _counter.Change -= ChangeText;
     }
 
-    private void ChangeTime()
+    private void ChangeText()
     {
-        if (_switch.IsOn)
-        {
-            var OnCounter =  StartCoroutine(TimeCounter());
-        }
-        else
-        {
-            StopAllCoroutines();
-        }
-    }
-
-    private IEnumerator TimeCounter()
-    {
-        WaitForSeconds delayTime = new WaitForSeconds(_delayTime);
-
-        while (true) 
-        {
-            _counter.TakeTeime(_delayTime);
-            _text.text = _counter.Count.ToString();
-
-            yield return delayTime;
-        }
+        _text.text = _counter.Count.ToString();
     }
 }
